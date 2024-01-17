e9d214718e312ae0cfd90904e11d77537123827e
Hongbo Wu <hongbo@omnivore.app>
2024-01-17 10:04:22 +0800

    revert soft deletes all the labels and highlights attached to the deleted item

--- packages/api/src/services/library_item.ts
+++ packages/api/src/services/library_item.ts
@@ -707,12 +707,6 @@ export const softDeleteLibraryItem = async (
         deletedAt: new Date(),
       })
 
-      // delete all labels for this item
-      await tx.getRepository(EntityLabel).delete({ libraryItemId: id })
-
-      // delete all highlights for this item
-      await tx.getRepository(Highlight).delete({ libraryItem: { id } })
-
       return itemRepo.findOneByOrFail({ id })
     },
     undefined,
@@ -734,13 +728,10 @@ export const updateLibraryItem = async (
     async (tx) => {
       const itemRepo = tx.withRepository(libraryItemRepository)
 
-      // reset deletedAt and archivedAt
+      // reset archivedAt
       switch (libraryItem.state) {
         case LibraryItemState.Archived:
           libraryItem.archivedAt = new Date()
-          break
-        case LibraryItemState.Deleted:
-          libraryItem.deletedAt = new Date()
           break
         case LibraryItemState.Processing:
         case LibraryItemState.Succeeded:
